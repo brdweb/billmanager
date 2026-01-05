227f92209526ecf60e29e4073f5939174a6011d0
brdweb <jasonmitchell@brdweb.com>
2026-01-05 09:16:07 -0500

    Simplify Metro config by removing monorepo-specific paths

--- apps/mobile/metro.config.js
+++ apps/mobile/metro.config.js
@@ -1,20 +1,6 @@
 const { getDefaultConfig } = require('expo/metro-config');
 const { withNativeWind } = require('nativewind/metro');
-const path = require('path');
 
-const projectRoot = __dirname;
-const monorepoRoot = path.resolve(projectRoot, '../..');
-const packagesRoot = path.resolve(monorepoRoot, 'packages');
-
-const config = getDefaultConfig(projectRoot);
-
-// Watch the packages directory for monorepo support
-config.watchFolders = [packagesRoot];
-
-// Let Metro know where to resolve packages
-config.resolver.nodeModulesPaths = [
-  path.resolve(projectRoot, 'node_modules'),
-  path.resolve(monorepoRoot, 'node_modules'),
-];
+const config = getDefaultConfig(__dirname);
 
 module.exports = withNativeWind(config, { input: './global.css' });
